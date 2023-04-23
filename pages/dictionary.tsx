0abb27b728f9f4a47ba0111f75f8ade801e5ad3c
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-04-23 21:21:53 +0200

    Refactor the getRandomWord - simpler code Add Todo list to dictionary page

--- pages/dictionary.tsx
+++ pages/dictionary.tsx
@@ -1,13 +1,30 @@
 import {getOptions} from "@/utils/getRandomWord";
 import {useEffect, useState} from "react";
+import {number} from "prop-types";
 
 
 export default function Dictionary() {
-    const [words, updateWords] = useState([]);
+    const [words, setWords] = useState([]);
+    const [choose, setChoose] = useState({or_id:null,tr_id:null});
+    const [count, setCount] = useState(0);
+
 
     useEffect(() => {
-        updateWords(getOptions())
-    },[])
+        setWords(getOptions())
+    },[]);
+    useEffect(() => {
+        //Todo: If count = 4 "Next Button" Revealed;
+    })
+
+    const handleClick = () => {
+        //Todo: If Click one of the divs, first checks whether you were previously selected
+        //Todo: If it was selected and the same: nothing happens
+        //Todo: If it was selected and is not the same: switch to the other word
+        //Todo: If it was selected and paired with another word, Tests whether the selection is good:
+            //Todo: If it's good, then the count increment by one, and empty the choose
+            //Todo: If it is wrong, it just empties the choose
+        //Todo: Feedback on UI with CSS formatting
+    }
 
     const [originalWords, translatedWords] = words;
 
--- utils/getRandomWord.ts
+++ utils/getRandomWord.ts
@@ -1,79 +1,50 @@
 import {DummyDatabase} from "@/DummyDatabase";
 
 
-
+//Creation of necessary variables
 const MAX_ID = DummyDatabase.length
-let notThisOne: number[] = new Array();
+const notThisOne: number[] = new Array();
+const originalWord: { id: number; original_word: string}[] = new Array();
+const translatedWord: { id: number; translated_word: string}[] = new Array();
 
 const getRandomWord = () => {
     const randomId = (Math.floor(Math.random() * (MAX_ID))+1)
     let newId = true;
     for(let i:number = 0; i<notThisOne.length; i++) {
         if (notThisOne[i] === randomId){
+            //This line only works if a larger database is available
+            //With a small database infinite recursion may occur
+            //In this case there is no need for newId-validation
+            /*getRandomWord()*/
             newId = false;
         }
     }
     if (newId){
         notThisOne.push(randomId);
+        originalWord.push({id:DummyDatabase[randomId-1].id, original_word:DummyDatabase[randomId-1].original_word})
+        translatedWord.push({id:DummyDatabase[randomId-1].id, translated_word:DummyDatabase[randomId-1].translated_word})
     }
 
 }
 
+//Main function for retrieving the necessary variables
 export const getOptions = () => {
     do {
         getRandomWord();
     } while (notThisOne.length!==4)
-    const originalWord: { id: number; original_word: string}[] = getOriginalWords();
-    const translatedWord: { id: number; translated_word: string}[] = getRandomTranslateWords();
-    return [originalWord,translatedWord];
+    return [originalWord,getRandom(translatedWord)];
 
 
 
 }
 
-function getOriginalWords() {
-    return [{
-        id:DummyDatabase[notThisOne[0]-1].id,
-        original_word:DummyDatabase[notThisOne[0]-1].original_word
-    },
-        {
-            id:DummyDatabase[notThisOne[1]-1].id,
-            original_word:DummyDatabase[notThisOne[1]-1].original_word
-        },
-        {
-            id:DummyDatabase[notThisOne[2]-1].id,
-            original_word:DummyDatabase[notThisOne[2]-1].original_word
-        },
-        {
-            id:DummyDatabase[notThisOne[3]-1].id,
-            original_word:DummyDatabase[notThisOne[3]-1].original_word
-        }];
-}
-
-const getRandomTranslateWords = () => {
-    let newNumberOrder = notThisOne;
-    for(let i = newNumberOrder.length-1; i>0; i--) {
+//Mixes the given array
+const getRandom = (translatedWord: { id: number; translated_word: string }[]) => {
+    for(let i = translatedWord.length-1; i>0; i--) {
         const j = Math.floor(Math.random() * (i+1))
-        const temp = newNumberOrder[i];
-        newNumberOrder[i] = newNumberOrder[j];
-        newNumberOrder[j] = temp;
+        const temp = translatedWord[i];
+        translatedWord[i] = translatedWord[j];
+        translatedWord[j] = temp;
     }
-    const translatedWord: { id: number; translated_word: string}[] =
-        [{
-            id:DummyDatabase[newNumberOrder[0]-1].id,
-            translated_word:DummyDatabase[newNumberOrder[0]-1].translated_word
-        },
-            {
-                id:DummyDatabase[newNumberOrder[1]-1].id,
-                translated_word:DummyDatabase[newNumberOrder[1]-1].translated_word
-            },
-            {
-                id:DummyDatabase[newNumberOrder[2]-1].id,
-                translated_word:DummyDatabase[newNumberOrder[2]-1].translated_word
-            },
-            {
-                id:DummyDatabase[newNumberOrder[3]-1].id,
-                translated_word:DummyDatabase[newNumberOrder[3]-1].translated_word
-            }];
     return translatedWord;
 }
