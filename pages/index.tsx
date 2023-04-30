5c55ce484c46ba831d5977e2d396b96c068959cf
Gudbjorg Vernhardsdottir <gudbjorgl@gmail.com>
2023-04-30 14:54:15 +0200

    added AccessDenied and SignOuButton components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import React from "react";
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import SignOutButton from "@/components/SignOutButton";
+import AccessDenied from "@/components/AccessDenied";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  console.log(session);
+
+
   return (
-    <>
-      
-    </>
+    <div>
+      Hello
+      {!session && (
+          <>
+            <AccessDenied />
+          </>
+      )}
+      {session && (
+          <>
+            <SignOutButton />
+          </>
+      )}
+
+    </div>
   )
 }
