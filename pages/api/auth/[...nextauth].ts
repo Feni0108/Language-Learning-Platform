485f8bca2eff804fcc270aee6217a617226daa46
Fanni Görföl <94fanni@gmail.com>
2023-05-11 21:37:19 +0200

    Use global prisma file in [...nextauth].ts file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,60 +1,59 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import {PrismaClient} from "@prisma/client";
-import {string} from "prop-types";
+import { prisma } from "../../../lib/prisma";
 
 const authOptions: NextAuthOptions = {
-    // set up 32 character long secret key in .env.local:
-    secret: process.env.NEXTAUTH_SECRET,
-    session: {
-        strategy: "jwt",
+  // set up 32 character long secret key in .env.local:
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      credentials: {},
+      authorize: async (credentials, req) => {
+        const { username, password } = credentials as {
+          username: string;
+          password: string;
+        };
+
+        // @ts-ignore
+        const user = await prisma.user.findUnique({
+          where: { username: username },
+          select: { username: true, password: true },
+        });
+        console.log(user);
+
+        // perform you login logic
+        // find out user from db
+        if (!user) {
+          throw new Error("User doesn't exists");
+        }
+
+        if (password == user.password) {
+          return {
+            username: username,
+          };
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    // error: '/auth/error',
+    // signOut: '/auth/signout'
+  },
+  callbacks: {
+    jwt(params) {
+      // update token
+      if (params.user?.role) {
+        params.token.role = params.user.role;
+      }
+      // return final_token
+      return params.token;
     },
-    providers: [
-        CredentialsProvider({
-            type: "credentials",
-            credentials: {},
-            authorize: async (credentials, req) => {
-                const { username, password } = credentials as {
-                    username: string;
-                    password: string;
-                };
-
-                const prisma = new PrismaClient();
-
-                // @ts-ignore
-                const user = await prisma.user.findUnique({where: {username: username}, select: {username: true, password: true} });
-                console.log(user);
-
-                // perform you login logic
-                // find out user from db
-                if (!user) {
-                    throw new Error("User doesn't exists");
-                }
-
-                if (password == user.password) {
-                    return {
-                        username: username,
-                    };
-                }
-            },
-        }),
-    ],
-    pages: {
-        signIn: "/auth/signin",
-        // error: '/auth/error',
-        // signOut: '/auth/signout'
-    },
-    callbacks: {
-        jwt(params) {
-            // update token
-            if (params.user?.role) {
-                params.token.role = params.user.role;
-            }
-            // return final_token
-            return params.token;
-        },
-    },
+  },
 };
 
 export default NextAuth(authOptions);
-
