1d67dc61e2ec3bfbb620c4d99cbed676c48e561e
Fanni Görföl <94fanni@gmail.com>
2023-05-11 21:38:00 +0200

    Use global prisma file in createUser.ts

--- pages/api/createUser.ts
+++ pages/api/createUser.ts
@@ -1,22 +1,23 @@
-import {PrismaClient} from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from "../../lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    let message;
+  let message;
 
-    const prisma = new PrismaClient();
-    if (req.method === "POST") {
-        const username = req.body.username;
-        const password = req.body.password;
-        try {
-            const newUser = await prisma.user.create({data: {username, password}});
-            return res.status(200).send(newUser);
-        } catch (e) {
-            message = "Username already exists"
-            return res.status(404).json({response: {message:  message}});
-        }
+  if (req.method === "POST") {
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+      const newUser = await prisma.user.create({
+        data: { username, password },
+      });
+      return res.status(200).send(newUser);
+    } catch (e) {
+      message = "Username already exists";
+      return res.status(404).json({ response: { message: message } });
     }
+  }
 }
