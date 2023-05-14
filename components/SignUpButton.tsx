0bc0fb90b68f486fdf4800973d42bc586ec411f4
Fanni Görföl <94fanni@gmail.com>
2023-05-14 13:33:25 +0200

    Change the style of the SignUpButton
    now it also includes the sign in button, because I wantet to use them together

--- components/SignUpButton.tsx
+++ components/SignUpButton.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import Link from "next/link"
-const SignUpButton = () => {
-    return (
-        <div>
-            <Link href="/auth/signup">
-                Sign up
-            </Link>
-        </div>
-    )
-}
+import { RxEnter } from "react-icons/rx";
 
-export default SignUpButton
+const SignUpButton = () => {
+  return (
+    <div className="mt-10 flex-inline">
+      <a
+        href="/auth/signup"
+        className="bg-teal-500 rounded-3xl py-3 px-8 font-medium inline-block 
+            mr-4 hover:bg-transparent hover:border-teal-500 hover:text-gray-800 
+            duration-300 hover:border border-transparent text-gray-100"
+      >
+        Sign up!
+      </a>
+      <a href="/auth/signin">
+        I already have an account!{" "}
+        <span className="text-xl inline-block ps-1 align-middle">
+          <RxEnter />
+        </span>
+      </a>
+    </div>
+  );
+};
+
+export default SignUpButton;
