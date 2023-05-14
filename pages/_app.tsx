305b636c2d2eac31118ff2c1382e439fbd3c8496
Fanni Görföl <94fanni@gmail.com>
2023-05-14 13:10:17 +0200

    Import globals.css in the app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
-import '@/styles/globals.css'
 import React from "react";
-import type { AppProps } from 'next/app'
-import {SessionProvider} from "next-auth/react";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css";
+import Navbar from "@/components/nav/navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <SessionProvider session={pageProps.session}>
-    <Component {...pageProps} />
-  </SessionProvider>
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
