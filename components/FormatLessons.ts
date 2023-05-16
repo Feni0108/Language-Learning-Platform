dd52e392bb15b2304642085ab655f96eed3ba8a3
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-05-16 20:16:17 +0200

    Add Tailwind to project

---
+++ components/FormatLessons.ts
@@ -0,0 +1,9 @@
+export const FormatLessons: {invisible: string, visible:string, choose:string, wrongAnswer: string} = {
+    invisible: "line-through text-green-600",
+    visible:"",
+    choose:"text-green-600 bg-lime-500",
+    wrongAnswer:"text-red-600 bg-rose-600"
+}
+
+
+
