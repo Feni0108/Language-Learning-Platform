2343ec2902bcc1e4b6a21f213f9f7cead72189e7
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-05-21 14:46:46 +0200

    Add next task button and handlesolved function, gamecounter

--- pages/lessons.tsx
+++ pages/lessons.tsx
@@ -1,4 +1,5 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+import styles from "@/styles/Senctence.module.css";
 
 
 const games = [
@@ -9,6 +10,8 @@ const games = [
 
 export default function Lessons() {
     const [game, setGame] = useState<object>(null);
+    const [solved, isSolved] = useState(true);
+    const [gameCount, setGameCount] = useState(0);
 
     const getRandomGames = () => {
         const randomId = (Math.floor(Math.random() * (games.length)))
@@ -19,9 +22,22 @@ export default function Lessons() {
         getRandomGames();
     });
 
+    const handleSolved = () => {
+        setGameCount(gameCount => gameCount+1);
+        getRandomGames();
+    }
+
     return (
         <div>
             {game}
+            <div>
+                This is your process: {gameCount}/10
+            </div>
+            {isSolved && <div>
+                <button
+                    onClick={() => handleSolved()}
+                >Next task</button>
+            </div>}
         </div>
     )
 }
