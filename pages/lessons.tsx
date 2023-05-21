7a698a68981254003020a17c8c6f44c7d2edbbfb
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-05-21 15:02:48 +0200

    Add finished variables, which change after ten games and finished the lesson

--- pages/lessons.tsx
+++ pages/lessons.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import styles from "@/styles/Senctence.module.css";
+import {set} from "yaml/dist/schema/yaml-1.1/set";
 
 
 const games = [
@@ -12,6 +13,9 @@ export default function Lessons() {
     const [game, setGame] = useState<object>(null);
     const [solved, isSolved] = useState(true);
     const [gameCount, setGameCount] = useState(0);
+    const [isGood, setIsGood] = useState(false);
+    const [point, setPoint] = useState(0);
+    const [isFinished, setIsFinished] = useState(false);
 
     const getRandomGames = () => {
         const randomId = (Math.floor(Math.random() * (games.length)))
@@ -24,19 +28,34 @@ export default function Lessons() {
 
     const handleSolved = () => {
         setGameCount(gameCount => gameCount+1);
-        getRandomGames();
+        if (isGood){
+            setPoint(point => point+1);
+            setIsGood(false);
+        }
+        if (gameCount === 10) {
+            setIsFinished(true);
+        } else getRandomGames();
     }
 
     return (
         <div>
-            {game}
+            {!isFinished &&
             <div>
                 This is your process: {gameCount}/10
-            </div>
-            {isSolved && <div>
-                <button
-                    onClick={() => handleSolved()}
-                >Next task</button>
+                {game}
+                {isSolved && <div>
+                    <button
+                        onClick={() => handleSolved()}
+                    >Continue</button>
+                </div>}
+            </div>}
+            {isFinished &&
+            <div>
+                <div>
+                    Congratulations! YOu finished the lesson!
+                    Your points: {point}
+                </div>
+                <button>Continue</button>
             </div>}
         </div>
     )
