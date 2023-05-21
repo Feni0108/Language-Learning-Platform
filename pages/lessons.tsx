fdfa496cc2bee54ebde3518b7e053e5cacbb9caf
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-05-21 15:08:44 +0200

    Add row counting!

--- pages/lessons.tsx
+++ pages/lessons.tsx
@@ -1,6 +1,4 @@
 import React, {useEffect, useState} from "react";
-import styles from "@/styles/Senctence.module.css";
-import {set} from "yaml/dist/schema/yaml-1.1/set";
 
 
 const games = [
@@ -16,6 +14,8 @@ export default function Lessons() {
     const [isGood, setIsGood] = useState(false);
     const [point, setPoint] = useState(0);
     const [isFinished, setIsFinished] = useState(false);
+    const [row, setRow] = useState(0);
+    const [isInRow, setIsInRow] = useState(true);
 
     const getRandomGames = () => {
         const randomId = (Math.floor(Math.random() * (games.length)))
@@ -30,7 +30,12 @@ export default function Lessons() {
         setGameCount(gameCount => gameCount+1);
         if (isGood){
             setPoint(point => point+1);
+            setIsInRow(true);
+            setRow(row => row+1);
             setIsGood(false);
+        } else {
+            setIsInRow(false);
+            setRow(0);
         }
         if (gameCount === 10) {
             setIsFinished(true);
@@ -42,6 +47,7 @@ export default function Lessons() {
             {!isFinished &&
             <div>
                 This is your process: {gameCount}/10
+                {isInRow && row > 1 && <p>{row} in a row!</p>}
                 {game}
                 {isSolved && <div>
                     <button
