4c11eb9e548959f34fd3da53649ed395a78f55ca
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-05-23 20:19:32 +0200

    Move dictionary and picturegame to the Component. Delete the getServerSideProps method

---
+++ components/games/dictionary/Word.tsx
@@ -0,0 +1,76 @@
+import {FormatLessons} from "@/components/FormatLessons";
+import React, {useEffect, useState} from "react";
+
+
+type Props = {
+    word: string;
+    isVisible: boolean
+
+    isSelected: boolean
+    id:number;
+    wordType:string;
+
+    fetchData: () => void;
+    handleClick: (id:number, wordType:string) => void;
+
+
+
+    isWrong: object;
+
+    setIsWrong: (val: object) => void;
+
+
+
+}
+
+const Word = ({
+    word,
+    wordType,
+    isVisible,
+    id,
+    fetchData,
+    isSelected,
+    handleClick,
+    isWrong,
+    setIsWrong
+
+}: Props) => {
+    const [style, setStyle] = useState<string>(null);
+
+    useEffect(() => {
+        if (isSelected){
+            setStyle(FormatLessons.choose)
+        } else {
+            setStyle(FormatLessons.visible)
+        }
+        if(!isVisible){
+            setStyle(FormatLessons.invisible)
+        }
+    },[isVisible, isSelected, isWrong]);
+
+    useEffect(() => {
+            if ((isWrong.or_id === id && wordType === "original") || (isWrong.tr_id === id && wordType === "translated")) {
+                setStyle(FormatLessons.wrongAnswer);
+                const interval = setInterval(() => {
+                    setIsWrong({or_id:null, tr_id:null})
+                    fetchData();
+                }, 1500);
+                return () => clearInterval(interval)
+            }
+    }, [isWrong])
+
+
+        return (
+            <div
+                className={style}
+                id={'orig_' + id.toString()}
+                onClick={isVisible ? () => handleClick(id, wordType) : null}
+            >
+                {word}
+            </div>
+        )
+
+}
+
+
+export default Word;
