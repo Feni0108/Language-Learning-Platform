71ac8d35dee67593e15f0787ee0776ae587182c2
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-05-25 10:11:43 +0200

    Create alternative Lesson page, and add useFetch to refetch the data

---
+++ pages/lessons2.tsx
@@ -0,0 +1,79 @@
+import React, {Component, useEffect, useState} from "react";
+import Dictionary from "@/components/games/dictionary/dictionary";
+import Picture from "@/components/games/picturegame/picture";
+import useFetch from "@/components/games/useFetch";
+
+
+
+export default function Lessons({allWords}) {
+    const [solved, isSolved] = useState(true);
+    const [gameCount, setGameCount] = useState(0);
+    const [isGood, setIsGood] = useState(false);
+    const [point, setPoint] = useState(0);
+    const [isFinished, setIsFinished] = useState(false);
+    const [row, setRow] = useState(0);
+    const [isInRow, setIsInRow] = useState(true);
+    const [randomId, setRandomId] = useState<number>(null);
+    const [word, setWord] = useState();
+    const [endpoint, setEndpoint] = useState('api/getGamesData/dictionary')
+    const {loading , pics}  = useFetch(endpoint);
+    const getRandomGames = () => {
+        const randomId = (Math.floor(Math.random() * 1))
+       setRandomId(randomId);
+    }
+
+    useEffect(() => {
+        console.log("Just for fun")
+        if(gameCount>3){
+            setEndpoint('api/getGamesData/picture')
+        }
+        if(gameCount>7){
+            setEndpoint('api/getGamesData/dictionary')
+        }
+    },[gameCount])
+
+
+    const handleSolved = () => {
+        setGameCount(gameCount => gameCount+1);
+        if (isGood){
+            setPoint(point => point+1);
+            setIsInRow(true);
+            setRow(row => row+1);
+            setIsGood(false);
+        } else {
+            setIsInRow(false);
+            setRow(0);
+        }
+        if (gameCount === 10) {
+            setIsFinished(true);
+        } else getRandomGames();
+    }
+
+    return (
+        <div>
+            {!isFinished &&
+            <div>
+                This is your process: {gameCount}/10
+                {isInRow && row > 1 && <p>{row} in a row!</p>}
+                <br/>
+                {randomId}
+                {console.log(word)}
+                {word && randomId === 1 && <Picture allWords={word} />}
+                {word && randomId === 0 && <Dictionary allWords={word} />}
+                {isSolved && <div>
+                    <button
+                        onClick={() => handleSolved()}
+                    >Continue</button>
+                </div>}
+            </div>}
+            {isFinished &&
+            <div>
+                <div>
+                    Congratulations! YOu finished the lesson!
+                    Your points: {point}
+                </div>
+                <button>Continue</button>
+            </div>}
+        </div>
+    )
+}
