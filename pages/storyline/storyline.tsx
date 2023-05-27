e1ff35d2e7f2033525615846bd00e71e5c6be98c
Gudbjorg Vernhardsdottir <gudbjorgl@gmail.com>
2023-05-27 21:06:49 +0200

    created getData and getServerSideProps functions

--- pages/storyline/storyline.tsx
+++ pages/storyline/storyline.tsx
@@ -1,15 +1,45 @@
 import React from "react"
 import { useState, useEffect } from 'react';
+import prisma from "@/lib/prisma";
+import { GetServerSideProps } from "next";
 
-const sentences = [
-    'Hello',
-    'My name is Gudda',
-    'What is your name?'
-];
+const getData = (storyline: any) => {
+    const getSentences = storyline[0].sentences.split(";");
+    const getWords = storyline[0].words.split(";");
 
-export default function Story() {
+    const sentences: any[] = [];
+    for (let i = 0; i < getSentences.length; i++ ){
+        sentences.push(getSentences[i]);
+    }
+
+    const words = [];
+    for (let i = 0; i < getWords.length; i++ ){
+        words.push(getWords[i]);
+    }
+
+    return {sentences, words};
+}
+
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+    const storyline = await prisma.storyline.findMany();
+    const data = getData(storyline)
+    return {
+        props: {
+            data: data,
+        }
+    }
+}
+
+export default function Story(props: any) {
     const [currentSentence, setCurrentSentence] = useState(0);
     const [displayedSentence, setDisplayedSentence] = useState<string[]>([]);
+
+
+    const sentences = props.data.sentences;
+    console.log(sentences)
+    
 
     useEffect(() => {
         if (currentSentence < sentences.length) {
