acdae003ae295c144f3e3968b05ce5fbd9fb8ce9
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-06-03 13:26:59 +0200

    Add message the userinterface, if the player don't play today

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,8 @@ export default function Home() {
               <h4>Your points: {session.user.totalPoints}</h4>
               {session.user?.username} <br />
               <br/>
-                         {session.user.strike > 0 && <h2>Ohh, yes! You are in {session.user.strike} strike!</h2>}
+            {session.user.strike > 0 && <h2>Ohh, yes! You are in {session.user.strike} strike!</h2>}
+            {session.user.strike > 0 && !isPlayToday && <h3>Duo sees a {session.user.strike+1}-day streak in your future. Will there be that many?</h3>}
               <Link href="lessons">Start game</Link>
             <SignOutButton />
           </>
