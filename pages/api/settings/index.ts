2473032b4ae9007956a9cb4cda630c105501d194
Gazda Zsolt <zgazda1010@gmail.com>
2023-06-04 14:04:24 +0200

    Implemented setting and saving user settings, display settings page for new users

---
+++ pages/api/settings/index.ts
@@ -0,0 +1,47 @@
+import {getSession} from 'next-auth/react';
+import prisma from '../../../lib/prisma';
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    return handleGetSettings(req, res);
+  }
+  if (req.method === 'POST') {
+    return handleSaveSettings(req, res);
+  }
+  res.status(405).end();
+}
+
+async function handleSaveSettings(req: NextApiRequest, res: NextApiResponse) {
+  const {interfaceLanguage, targetLanguage, learningGoal, userId} = req.body;
+  try {
+    const userSettings = await prisma.userSettings.upsert({
+      where: {userId},
+      update: {interfaceLanguage, targetLanguage, learningGoal},
+      create: {userId, interfaceLanguage, targetLanguage, learningGoal, updatedAt: new Date(),},
+    });
+    res.status(200).json({userSettings});
+  } catch (error) {
+    res.status(500).json({error: 'Failed to save settings'});
+  }
+}
+
+async function handleGetSettings(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({req});
+  if (!session || !session.user) {
+    return res.status(401).json({error: 'Unauthorized'});
+  }
+
+  const userId = session.user.id;
+  try {
+    const userSettings = await prisma.userSettings.findUnique({
+      where: {userId},
+    });
+    if (!userSettings) {
+      return res.status(200).json({userSettings: null});
+    }
+    res.status(200).json({userSettings});
+  } catch (error) {
+    res.status(500).json({error: 'Failed to get settings'});
+  }
+}
