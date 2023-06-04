2e1eaf1af04e47e1602acb222a73a53ec9e8dc0a
Gudbjorg Vernhardsdottir <gudbjorgl@gmail.com>
2023-06-04 11:32:15 +0200

    added a field for typing password again

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -98,6 +98,32 @@ const SignUp = () => {
                 type="password"
                 placeholder="********"
               />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Password Again
+            </label>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 
+                text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 
+                placeholder:text-gray-400 focus:ring-2 focus:ring-inset 
+                focus:ring-indigo-600 sm:text-sm sm:leading-6 ps-5"
+                value={userInfo.password}
+                onChange={({ target }) =>
+                  setUserInfo({ ...userInfo, password: target.value })
+                }
+                type="password"
+                placeholder="********"
+              />
               <input
                 type="submit"
                 className="flex w-full justify-center rounded-md 
