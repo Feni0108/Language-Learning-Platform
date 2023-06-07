c197bf3e81558a5d0a5ad8cf54546a8bbedd4632
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-06-07 21:21:57 +0200

    Add progressupdate to updatePoints (2.)

--- components/updatePoints.ts
+++ components/updatePoints.ts
@@ -1,7 +1,7 @@
 import {router} from "next/client";
 import {EventHandler, useState} from "react";
 
-export const updatePoints = async (totalPoints: number, userId:string) => {
+export const updatePoints = async (totalPoints: number, userId:string, isProgressUpdate:boolean) => {
     const endpoint = "http://localhost:3000/api/updatePoints";
     console.log("In apiy");
     console.log(userId);
@@ -12,16 +12,12 @@ export const updatePoints = async (totalPoints: number, userId:string) => {
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
                 userId: userId,
-                points: totalPoints
+                points: totalPoints,
+                isProgressUpdate: isProgressUpdate
             })
         }
         const res = await fetch(endpoint, postData);
         const response = await res.json();
-        console.log(response);
-        console.log(typeof response);
-        /*if (typeof response === "object") {
-            await router.push("http://localhost:3000/");
-        }*/
 
     } catch (e) {
         console.log(e);
