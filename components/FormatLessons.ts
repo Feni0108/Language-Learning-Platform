3cdf9e3665db0ef541451cf2354acd5bbc3829d4
Fanni Görföl <94fanni@gmail.com>
2023-06-08 19:44:28 +0200

    Change the styling in FormatLessons.ts for Word component

--- components/FormatLessons.ts
+++ components/FormatLessons.ts
@@ -1,10 +1,17 @@
-export const FormatLessons: {invisible: string, visible:string, choose:string, wrongAnswer: string, goodAnswer:string} = {
-    invisible: "line-through text-green-600",
-    visible:"",
-    choose:"text-green-600 bg-cyan-300",
-    wrongAnswer:"text-red-600 bg-rose-600",
-    goodAnswer: "text-green-600 bg-lime-600",
-}
-
-
-
+export const FormatLessons: {
+  invisible: string;
+  visible: string;
+  choose: string;
+  wrongAnswer: string;
+  goodAnswer: string;
+} = {
+  invisible:
+    "p-5 m-5 text-center w-56 rounded md:rounded-full border-2 hover:border-4 bg-teal-400 text-white",
+  visible:
+    "p-5 m-5 text-center w-56 rounded md:rounded-full border-2 hover:border-4",
+  choose:
+    "p-5 m-5 text-center w-56 rounded md:rounded-full border-2 hover:border-4 text-teal-700",
+  wrongAnswer:
+    "p-5 m-5 text-center w-56 rounded md:rounded-full border-2 hover:border-4 bg-red-200 text-white",
+  goodAnswer: "flex justify-center m-10",
+};
