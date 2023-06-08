19e24425a4307dfcc5d805bb2d6546280c4b3930
Fanni Görföl <94fanni@gmail.com>
2023-06-08 14:21:07 +0200

    Add comment to know how to use moment and change tabulation

--- components/testDate.ts
+++ components/testDate.ts
@@ -1,31 +1,33 @@
 import prisma from "@/lib/prisma";
+//npm install moment --save
 import moment from "moment";
 
-export const testDate = (prevDate: Date, id: string) : boolean => {
-    if (prevDate == null){
-        return false;
-    }
-    const prevDay = moment(prevDate).startOf('day');
-    const day = moment().startOf('day');
+export const testDate = (prevDate: Date, id: string): boolean => {
+  if (prevDate == null) {
+    return false;
+  }
+  const prevDay = moment(prevDate).startOf("day");
+  const day = moment().startOf("day");
 
-    if (day - prevDay == 0){
-        return true;
-    } else {
-        if (day-prevDay == 86400000){
-            return false;
-        } else deleteStrike(id).then((res) => {return false})
-    }
-}
+  if (day - prevDay == 0) {
+    return true;
+  } else {
+    if (day - prevDay == 86400000) {
+      return false;
+    } else
+      deleteStrike(id).then((res) => {
+        return false;
+      });
+  }
+};
 
 export const deleteStrike = async (id: string) => {
-    const updatePoints = await prisma.leaderboard.update(
-        {
-            where: {
-                userId: id,
-            },
-            data: {
-                strike: 0
-            }
-        }
-    );
-}
+  const updatePoints = await prisma.leaderboard.update({
+    where: {
+      userId: id,
+    },
+    data: {
+      strike: 0,
+    },
+  });
+};
