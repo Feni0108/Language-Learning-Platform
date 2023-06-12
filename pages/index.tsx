d290262f6f77e61ddb410f8f021d821624f16367
Fanni Görföl <94fanni@gmail.com>
2023-06-12 21:35:19 +0200

    Add style to the index page after sign in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-
-import React, {useEffect, useState} from "react";
-import { Inter } from 'next/font/google'
+import React, { useEffect, useState } from "react";
+import { Inter } from "next/font/google";
 import { useSession } from "next-auth/react";
 import SignUpButton from "@/components/SignUpButton";
 import Link from "next/link";
-import {lastGame} from "@/components/lastGame";
-import {useRouter} from "next/router";
+import { lastGame } from "@/components/lastGame";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,28 +19,28 @@ export default function Home() {
         setIsPlayToday(res);
       });
     }
-  }, [session])
+  }, [session]);
 
   useEffect(() => {
     if (session) {
-      update({id: session.user.id})
+      update({ id: session.user.id });
     }
-  }, [isPlayToday])
+  }, [isPlayToday]);
 
   const router = useRouter();
 
-
   if (session) {
     console.log(session);
-    if (session?.user?.interfaceLanguage === undefined &&
-        session.user?.learningGoal === undefined &&
-        session.user?.targetLanguage === undefined) {
+    if (
+      session?.user?.interfaceLanguage === undefined &&
+      session.user?.learningGoal === undefined &&
+      session.user?.targetLanguage === undefined
+    ) {
       router.push("http://localhost:3000/settings");
     }
   }
 
   return (
-
     <>
       {!session && (
         <div className="text-gray-700 max-w-5xl px-20 py-28">
@@ -72,16 +71,29 @@ export default function Home() {
             </span>
           </h1>
           <div>
-           
-            <h4>Your points: {session.user.totalPoints}</h4>
-            {session.user?.username} <br />
-            <br/>
-            {session.user.strike > 0 && <h2>Ohh, yes! You are in {session.user.strike} strike!</h2>}
-            {session.user.strike > 0 && !isPlayToday && <h3>Duo sees a {session.user.strike+1}-day streak in your future. Will there be that many?</h3>}
-            <Link href="lessons">Start game</Link>
+            <p className="m-5 text-xl">
+              Your points: {session.user.totalPoints}
+            </p>
+            <br />
+            {session.user.strike > 0 && (
+              <h2>Ohh, yes! You are in {session.user.strike} strike!</h2>
+            )}
+            {session.user.strike > 0 && !isPlayToday && (
+              <h3>
+                Duo sees a {session.user.strike + 1}-day streak in your future.
+                Will there be that many?
+              </h3>
+            )}
+            <Link
+              href="lessons"
+              className="bg-teal-500 rounded-3xl py-3 px-8 font-medium inline-block 
+            mr-4 hover:bg-transparent hover:border-teal-500 hover:text-gray-800 
+            duration-300 hover:border border-transparent text-gray-100"
+            >
+              Start game
+            </Link>
           </div>
         </div>
-
       )}
     </>
   );
