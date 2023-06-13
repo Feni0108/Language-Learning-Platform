47210a27886df0430856d765852f3797ea0f1481
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-06-13 11:44:55 +0200

    Fix Ts. errors

--- pages/registrationSuccess.tsx
+++ pages/registrationSuccess.tsx
@@ -2,14 +2,32 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { signIn } from "next-auth/react";
 
+
+type User = {
+  username?: string,
+  password?: string,
+  passwordAgain?: string,
+  isFirstLogin?: boolean
+}
+
+
 const RegistrationSuccess = () => {
   const router = useRouter();
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
     const { user } = router.query;
+    console.log(user);
     if (user) {
-      const parsedUser = JSON.parse(user);
+      let parsedUser : User= JSON.parse(user,  function (key, value) {
+       if (key === 'username') return value;
+       if (key === 'password') return value;
+        if (key === 'passwordAgain') return value;
+        if (key === 'isFirstLogin') return value;
+        return value;
+      })
+      console.log(parsedUser);
+      console.log(typeof  parsedUser);
       setUser(parsedUser);
     }
   }, [router.query]);
@@ -20,12 +38,12 @@ const RegistrationSuccess = () => {
 
     // Perform automatic login
     const result = await signIn("credentials", {
-      username: user.username,
-      password: user.password,
+      username: user!.username,
+      password: user!.password,
       redirect: false,
     });
 
-    if (result.ok) {
+    if (result!.ok) {
       router.push("/");
     } else {
       console.error("Failed to log in.");
@@ -40,7 +58,7 @@ const RegistrationSuccess = () => {
         <>
           <p>Username: {user.username}</p>
           <p>Password: {user.password}</p>
-          <p>isFirstLogin: {user.isFirstLogin.toString()}</p>
+          <p>isFirstLogin: {user.isFirstLogin!.toString()}</p>
           <button onClick={handleButtonClick}>Update isFirstLogin</button>
         </>
       )}
