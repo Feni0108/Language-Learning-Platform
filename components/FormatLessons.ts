7b43ce9751b23a9b5475086b1d87cca371ae0f74
Fanni Görföl <94fanni@gmail.com>
2023-06-13 20:58:28 +0200

    Add new styling constants to FormatLessons

--- components/FormatLessons.ts
+++ components/FormatLessons.ts
@@ -4,6 +4,17 @@ export const FormatLessons: {
   choose: string;
   wrongAnswer: string;
   goodAnswer: string;
+  sentenceWordBeforeUsing: string;
+  sentenceInvisible: string;
+  sentenceAnswer: string;
+  sentenceWordAfterUsing: string;
+  sentenceGoodAnswer: string;
+  sentenceWrongAnswer: string;
+  pictureGameImage: string;
+  pictureGameVisible: string;
+  pictureGameChoose: string;
+  pictureGameGoodAnswer: string;
+  pictureGameWrongAnswer: string;
 } = {
   invisible:
     "lg:p-5 mt-5 md:p-3 sm:p-2 text-center w-56 rounded rounded-full border-2 hover:border-4 bg-teal-400 text-white text-xs sm:text-base md:text-m",
@@ -13,5 +24,20 @@ export const FormatLessons: {
     "lg:p-5 mt-5 md:p-3 sm:p-2 text-center w-56 rounded rounded-full border-2 hover:border-4 text-teal-700 text-xs sm:text-base md:text-m",
   wrongAnswer:
     "lg:p-5 mt-5 md:p-3 sm:p-2 text-center w-56 rounded rounded-full border-2 hover:border-4 bg-red-200 text-white text-xs sm:text-base md:text-m",
-  goodAnswer: "flex justify-center m-10",
+  goodAnswer:
+    "justify-center lg:p-5 mt-5 md:p-3 sm:p-2 text-center w-56 rounded rounded-full border-2 hover:border-4 bg-teal-400 text-white text-xs sm:text-base md:text-m",
+  sentenceWordBeforeUsing:
+    "display: inline p-5 m-5 rounded rounded-full border-2 hover:border-4",
+  sentenceAnswer:
+    "flex flex-inline justify-center mt-10 mb-10 border-dotted border-b-[3px] w-2/4",
+  sentenceInvisible:
+    "display: inline p-5 m-5 rounded rounded-full border-2 text-white",
+  sentenceWordAfterUsing: "p-5",
+  sentenceGoodAnswer: "mt-12 text-center text-teal-600",
+  sentenceWrongAnswer: "mt-12 text-center text-red-600",
+  pictureGameImage: "m-8",
+  pictureGameVisible: "hover:text-xl",
+  pictureGameChoose: "text-teal-600 text-xl",
+  pictureGameGoodAnswer: "mt-12 text-center text-teal-600",
+  pictureGameWrongAnswer: "mt-12 text-center text-red-600",
 };
