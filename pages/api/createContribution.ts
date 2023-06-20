b501b6f728ff4a86668a6215405e0143b237ec6d
Fanni Görföl <94fanni@gmail.com>
2023-06-20 18:13:22 +0200

    Create API endpoint to be able to create new Contribution elements

---
+++ pages/api/createContribution.ts
@@ -0,0 +1,33 @@
+import { prisma } from "../../lib/prisma";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function saveNewWord(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let message;
+
+  if (req.method === "POST") {
+    const word = req.body.word;
+    const language = req.body.language;
+    const description = req.body.description;
+    const category = req.body.category;
+
+    console.log(word, language, description, category);
+
+    try {
+      const newWord = await prisma.contribution.create({
+        data: {
+          word,
+          language,
+          description,
+          category,
+        },
+      });
+      return res.status(200).send(newWord);
+    } catch (e) {
+      message = "Word already exists";
+      return res.status(404).json({ response: { message: message } });
+    }
+  }
+}
