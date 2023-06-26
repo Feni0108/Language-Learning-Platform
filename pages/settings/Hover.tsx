7b087ea58e16026c95cf44428d7efa446a3fe9fc
Király-Gál Gábor <ggabesz1111@gmail.com>
2023-06-26 20:31:28 +0200

    Add image to Hoverpage

--- pages/settings/Hover.tsx
+++ pages/settings/Hover.tsx
@@ -31,18 +31,28 @@ const Hover = ({
             {isShown ? (<div
                 onClick={() => handleSaveSettingsFromShortcut(hover.interfaceLanguage, hover.targetLanguage)}
                              onMouseLeave={() => setIsShown(false)}>
-                {hover.interfaceLanguage+"-"+hover.targetLanguage+"-icons"}
+                <img className="h-12 w-12 rounded-full" src={"https://cdn.icon-icons.com/icons2/2468/PNG/512/user_kids_avatar_user_profile_icon_149314.png"}/>
             </div>) : (<div className="pt-6"
                onMouseEnter={() => setIsShown(true)}
                             onClick={() => handleSaveSettingsFromShortcut(hover.interfaceLanguage, hover.targetLanguage)}
                >
-                {hover.interfaceLanguage+"-"+hover.targetLanguage+"-icons"}
+                <img className="h-12 w-12 rounded-full" src={"https://cdn.icon-icons.com/icons2/2468/PNG/512/user_kids_avatar_user_profile_icon_149314.png"}/>
             </div>)}
 
         </>
     )
 
 }
-
+/*
+* <img
+                                          className="h-12 w-12 rounded-full"
+                                          src={
+                                            session.user?.image
+                                                ? session.user?.image
+                                                : "https://cdn.icon-icons.com/icons2/2468/PNG/512/user_kids_avatar_user_profile_icon_149314.png"
+                                          }
+                                          alt=""
+                                      />
+* */
 
 export default Hover;
